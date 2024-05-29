8d21e2697a84caa12d7033a88813dc47eedd5367
Florian Eich <florian.eich@bmc-labs.com>
2024-05-29 21:00:47 +0200

    chore: cleanup and fix of broken default config path

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+// Copyright 2024 bmc::labs GmbH. All rights reserved.
+
+pub const DEFAULT_DATABASE_URL: &str = "sqlite::memory:";
+pub const DEFAULT_CONFIG_PATH: &str = "/tmp/gitlab-runner/config.toml";
