a163d61bff76331b32b555c7cc8fb5b5459ac3c6
Florian Eich <florian.eich@bmc-labs.com>
2024-06-18 01:44:55 +0200

    Fix database migrations

--- migrations/20240203134208_gitlab_runners.up.sql
+++ migrations/20240203134208_gitlab_runners.up.sql
@@ -4,7 +4,5 @@ CREATE TABLE IF NOT EXISTS gitlab_runners (
     name         TEXT NOT NULL,
     url          TEXT NOT NULL,
     token        TEXT UNIQUE NOT NULL,
-    docker_image TEXT NOT NULL,
-    tag_list     TEXT NOT NULL,
-    run_untagged BOOLEAN NOT NULL
+    docker_image TEXT NOT NULL
 );
