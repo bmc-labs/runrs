e6fee79288397785e6f88c48e8e5cb91f494b9aa
Florian Eich <florian.eich@bmc-labs.com>
2024-06-25 01:15:53 +0200

    Start glrcfg impl using typed-builder

--- glrcfg/src/global_section.rs
+++ glrcfg/src/global_section.rs
@@ -1,20 +1,20 @@
 // Copyright 2024 bmc::labs GmbH. All rights reserved.
 
 use serde::Serialize;
+use typed_builder::TypedBuilder;
 
 /// These settings are global. They apply to all runners. Documentation of fields found here:
 /// https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-global-section
-#[derive(Debug, Serialize)]
+#[derive(Debug, TypedBuilder, Serialize)]
 pub struct GlobalSection {
+    #[builder(default = 1)]
     pub concurrent: u32,
+    #[builder(default = 3)]
     pub check_interval: u32,
 }
 
 impl Default for GlobalSection {
     fn default() -> Self {
-        Self {
-            concurrent: 4,
-            check_interval: 3,
-        }
+        Self::builder().build()
     }
 }
